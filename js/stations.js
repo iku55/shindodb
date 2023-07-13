8a3d931aadedc57e24a2d99ffb5973ad5baacb62
iku55 <ikscratch27@gmail.com>
2023-07-13 17:20:04 +0900

    震度観測点データ更新

--- js/stations.js
+++ js/stations.js
@@ -1,5 +1,7 @@
 function getStationData(date) {
-    if (date.getTime() >= new Date('2023-03-16T03:00:00.000Z').getTime()) {
+    if (date.getTime() >= new Date('2023-07-13T03:00:00.000Z').getTime()) {
+        return fetch('https://raw.githubusercontent.com/iku55/jma_int_stations/main/history/20230713.json');
+    } else if (date.getTime() >= new Date('2023-03-16T03:00:00.000Z').getTime()) {
         return fetch('https://raw.githubusercontent.com/iku55/jma_int_stations/main/history/20230316.json');
     } else if (date.getTime() >= new Date('2022-11-24T03:00:00.000Z').getTime()) {
         return fetch('https://raw.githubusercontent.com/iku55/jma_int_stations/main/history/20221124.json');
